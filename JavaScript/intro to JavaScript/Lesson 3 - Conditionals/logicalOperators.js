5a5127e5dfb22f41786d0bf157e4362f7cf7ea6c
EC2 Default User <ec2-user@ip-172-31-3-150.us-east-2.compute.internal>
2022-04-21 01:59:29 +0000

    Attempting Murder Mystery quiz. It's making my head spin.

---
+++ JavaScript/intro to JavaScript/Lesson 3 - Conditionals/logicalOperators.js
@@ -0,0 +1,16 @@
+var colt = "not busy";
+var weather = "nice";
+
+if (colt === "not busy" && weather === "nice") {
+    console.log("Go to the park");
+}
+
+/* && is a logical operator for AND; it's used to combine two logical expressions into one larger expression.
+
+If both expressions are true, then the entire expression evaluates to true.
+
+If either of these are false, then the whole logical expression is false.
+
+When you apply the ! operator to a boolean value, the ! returns true if the value is false and returns false if the value if true.
+
+*/
